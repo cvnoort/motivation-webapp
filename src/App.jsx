5ce0b78bd2172d3409cd99e6a2e13456e0ae136e
Christon S. <chadchriston@yahoo.com>
2025-01-23 15:08:52 -0500

    Reapply "Changes made to button stuff"
    This reverts commit 02e83ae57a10a26bfdc9e4d749f4239c2a44e554.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css';
+import './index.css';
 import TextQuotes from './TextQuotes';
+import React, { useState } from 'react';
 
 function App() {
+  const [category, setCategory] = useState('all');
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+  
   return (
     <> 
-      <TextQuotes/>
+      <select value={category} onChange={handleCategoryChange}>
+        <option value="all">All</option>
+        <option value="motivational">Motivational</option>
+        <option value="inspirational">Inspirational</option>
+      </select>
+      <TextQuotes category={category}/> 
     </>
   );
 }
