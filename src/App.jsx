02e83ae57a10a26bfdc9e4d749f4239c2a44e554
Christon S. <chadchriston@yahoo.com>
2025-01-23 15:08:07 -0500

    Revert "Changes made to button stuff"
    This reverts commit 6b496211a0bb6b1f6eef587508adebb8e0c04da9.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import './App.css';
-import './index.css';
 import TextQuotes from './TextQuotes';
-import React, { useState } from 'react';
 
 function App() {
-  const [category, setCategory] = useState('all');
-
-  const handleCategoryChange = (event) => {
-    setCategory(event.target.value);
-  };
-  
   return (
     <> 
-      <select value={category} onChange={handleCategoryChange}>
-        <option value="all">All</option>
-        <option value="motivational">Motivational</option>
-        <option value="inspirational">Inspirational</option>
-      </select>
-      <TextQuotes category={category}/> 
+      <TextQuotes/>
     </>
   );
 }
