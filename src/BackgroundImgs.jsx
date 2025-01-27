b2bc6cb37fcdd871dbe52093679e13fe80feb49c
Charlotte van Noort <61543485+cvnoort@users.noreply.github.com>
2025-01-27 18:40:07 +0100

    Remove unused import

--- src/BackgroundImgs.jsx
+++ src/BackgroundImgs.jsx
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from 'react';
-
 function BGImg() {
     const randomImage = "/motivation-webapp/images/background-" + (Math.floor(Math.random() * 15) + 1).toString() + ".jpg";
     return randomImage;
